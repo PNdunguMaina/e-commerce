6d5c5d6519e580f9558d5f1987963ec7a5646bda
PNdunguMaina <ndunguwamaina@gmail.com>
2022-09-15 13:28:01 +0300

    Implement navbar styles

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,11 +1,54 @@
-import React from 'react';
+/* eslint-disable object-curly-newline */
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Navbar, Container, Form, Button } from 'react-bootstrap';
 
-function Navigation() {
+import { fetchProducts } from '../../redux/products/productsSlice';
+
+const Navigation = () => {
+  const [setProduct] = useState('');
+  const dispatch = useDispatch();
+  const onChangeHandler = (e) => {
+    dispatch(fetchProducts(e.target.value));
+  };
+
+  const onClickHandler = () => {
+    dispatch(fetchProducts(''));
+    setProduct('');
+  };
+
   return (
-    <div className="nav-container">
-      <h1>NYX PRODUCTS CATALOGUE</h1>
-    </div>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand>
+          <img
+            src="../../images/NYX.png"
+            alt="NYX logo"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
+        </Navbar.Brand>
+
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search..."
+            className="me-2"
+            aria-label="Search"
+            onClick={onChangeHandler}
+          />
+          <Button
+            type="submit"
+            variant="outline-success"
+            onClick={onClickHandler}
+          >
+            Search
+          </Button>
+        </Form>
+      </Container>
+    </Navbar>
   );
-}
+};
 
 export default Navigation;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 import store from './redux/configureStore';
 
